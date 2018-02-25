04a317e18d3bb8e88d74efc8a8697178d27f6d48
BuildTools <jromphf@library.rochester.edu>
2018-02-25 00:52:40 -0500

    native events working now

--- index.js
+++ index.js
@@ -1,44 +1,66 @@
 // a native Bukkit event
-var ScriptCraftEvent = Java.type('org.eventdispatcher.events.scriptcraft.ScriptCraftEventWrapper');
+
 var events = require('events');
 var EventDispatcher = function() {
   this.queue = {};
   this.dispatched = {};
-  this.nativeEvent = function(name) { return new ScriptCraftEvent(name); }
-  this.nativeEventClass = ScriptCraftEvent;
+  try {
+    var ScriptCraftEvent = Java.type('org.eventdispatcher.events.scriptcraft.ScriptCraftEventWrapper');
+    this.nativeEvent = function(name) { return new ScriptCraftEvent(name); }
+    this.nativeEventClass = ScriptCraftEvent;
+  } catch(error) {
+    // no native event support
+    console.log("Warning: EventDispatcher constructed without native support. To enable, move event-dispatcher.jar to SpigotMC's plugins folder");
+    this.nativeEvent = null;
+    this.nativeEventClass = null;
+  }
 }
 
 EventDispatcher.prototype.on = function(eventType, callback) {
-  if (this.dispatched[eventType] === true) {
-    return callback();
+  if (events[eventType] !== undefined) {
+    events[eventType](callback);
+  } else {
+    if (this.dispatched[eventType] === true) {
+      return callback();
+    }
+    if (this.queue[eventType] === undefined) {
+      this.queue[eventType] = [];
+    }
+    this.queue[eventType].push(callback);
   }
-  if (this.queue[eventType] === undefined) {
-    this.queue[eventType] = [];
-  }
-  this.queue[eventType].push(callback);
 }
 
 EventDispatcher.prototype.dispatch = function(eventType, eventData) {
   if (this.queue[eventType] === undefined) {
     return;
   }
-  var nativeEvent = this.nativeEvent;
   this.queue[eventType].forEach(function(callback) {
     callback(eventData);
-    server.getPluginManager().callEvent(nativeEvent('test').getEvent());
-  });
+    if (this.nativeEvent !== null) {
+      var ne = this.nativeEvent(eventType).getEvent();
+      ne.data = eventData;
+      server.getPluginManager().callEvent(ne);
+    }
+  }.bind(this));
   this.queue[eventType] = [];
   this.dispatched[eventType] = true;
 }
 
 function testCustomEvent() {
   var eventSystem = new EventDispatcher();
+  // we can add handlers for all events, as well as our own js events
   eventSystem.on('test', function(event) {
+    console.log('Nashorn Event');
     console.log(event.message);
   });
-  events.on(eventSystem.nativeEventClass, function() {
-    console.log("this also worked");
-  });
+  // native plugins can listen to our events via ScriptCraftEvent, which has eventType + data
+  if (eventSystem.nativeEventClass !== null) {
+    events.on(eventSystem.nativeEventClass, function(event) {
+      console.log('Native Event');
+      console.log(event.eventType);
+      console.log(event.data.message);
+    });
+  }
   eventSystem.dispatch('test', { message: "this worked"});
 }
 
--- src/main/java/org/eventdispatcher/events/scriptcraft/ScriptCraftEvent.java
+++ src/main/java/org/eventdispatcher/events/scriptcraft/ScriptCraftEvent.java
@@ -3,9 +3,12 @@ package org.eventdispatcher.events.scriptcraft;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Cancellable;
+import org.eventdispatcher.events.scriptcraft.ScriptCraftEventData;
+import jdk.nashorn.api.scripting.JSObject;
 
 public class ScriptCraftEvent extends Event implements Cancellable {
   public String eventType;
+  public JSObject data;
   private static final HandlerList handlers = new HandlerList();
   private boolean isCancelled;
 
@@ -20,6 +23,14 @@ public class ScriptCraftEvent extends Event implements Cancellable {
 
   public void setCancelled(boolean isCancelled) {
     this.isCancelled = isCancelled;
+  }
+
+  public JSObject getData() {
+    return this.data;
+  }
+
+  public void setData(JSObject data) {
+    this.data = data;
   }
 
   public HandlerList getHandlers() {
